ca436cfcffcd4e48bbe5abdb5ecd7665e9fd8f4a
JuanCodeMaster <82987809+JuanCodeMaster@users.noreply.github.com>
2021-05-29 16:17:06 -0500

    Revert "Arreglado registro cliente"
    This reverts commit 89bb0159cdd4cd5c1ed173f689ab4fef79a1f467.

--- sistema/registro_cliente.php
+++ sistema/registro_cliente.php
@@ -8,6 +8,7 @@ if (!empty($_POST)) {
                         Todo los campos son obligatorios
                     </div>';
     } else {
+<<<<<<< HEAD
         $id = $_POST['id'];
         $año = $_POST['año'];
         $etapa_del_año = $_POST['etapa_del_año'];
@@ -19,6 +20,21 @@ if (!empty($_POST)) {
             $query = mysqli_query($conexion, "SELECT * FROM cliente where id = '$id'");
             $result = mysqli_fetch_array($query);
         }
+=======
+        $proveedor = $_POST['proveedor'];
+        $contacto = $_POST['contacto'];
+        $telefono = $_POST['telefono'];
+        $Direccion = $_POST['direccion'];
+        $usuario_id = $_SESSION['idUser'];
+        $modalidad = $_POST['modalidad'];
+        $intervencion = $_POST['intervencion'];
+        $avance = $_POST['avance'];
+        $estado = $_POST['estado'];
+
+        $query = mysqli_query($conexion, "SELECT * FROM proveedor where contacto = '$contacto'");
+        $result = mysqli_fetch_array($query);
+
+>>>>>>> 481afb0eeb9ca0542301cae91c3625376d91be0e
         if ($result > 0) {
             $alert = '<div class="alert alert-danger" role="alert">
                         El Ruc ya esta registrado
@@ -32,6 +48,7 @@ if (!empty($_POST)) {
                         Supervisión Registrado
                     </div>';
         } else {
+<<<<<<< HEAD
             $query_insert = mysqli_query($conexion, "INSERT INTO cliente(id,año,etapa_del_año,avance,estado) values ('$id', '$año', '$etapa_del_año', '$avance', '$estado')");
             if ($query_insert) {
                 $alert = '<div class="alert alert-primary" role="alert">
@@ -42,9 +59,14 @@ if (!empty($_POST)) {
                                     Error al Guardar
                             </div>';
             }
+=======
+            $alert = '<div class="alert alert-danger" role="alert">
+                       Error al registrar supervisón
+                    </div>';
+        }
+>>>>>>> 481afb0eeb9ca0542301cae91c3625376d91be0e
         }
     }
-}
 }
 mysqli_close($conexion);
 ?>
@@ -58,6 +80,7 @@ mysqli_close($conexion);
                 Registro de Supervisión
             </div>
             <div class="card">
+<<<<<<< HEAD
                 <div class="card-header bg-primary">
                     Nueva Autoevaluación
                 </div>
@@ -83,9 +106,49 @@ mysqli_close($conexion);
                         <input type="submit" value="Guardar Cliente" class="btn btn-primary">
                     </form>
                 </div>
+=======
+                <form action="" autocomplete="off" method="post" class="card-body p-2">
+                    <?php echo isset($alert) ? $alert : ''; ?>
+                    <div class="form-group">
+                        <label for="nombre">PERIODO</label>
+                        <input type="text" placeholder="Ingrese periodo" name="proveedor" id="nombre" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="contacto">AÑO</label>
+                        <input type="text" placeholder="Ingrese el año" name="contacto" id="contacto" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="telefono">ENTIDAD</label>
+                        <input type="text" placeholder="Ingrese la entidad" name="telefono" id="telefono" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="direccion">TIPO DE SUPERVISIÓN</label>
+                        <input type="text" placeholder="Ingrese el tipo de supervisión" name="direccion" id="direcion" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="modalidad">MODALIDAD</label>
+                        <input type="text" placeholder="Ingrese la modalidad" name="modalidad" id="direcion" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="intervencion">TIPO DE INTERVENCIÓN</label>
+                        <input type="text" placeholder="Ingrese el tipo de intervención" name="intervencion" id="direcion" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="avance">AVANCE</label>
+                        <input type="text" placeholder="Ingrese el avance" name="avance" id="direcion" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="estado">ESTADO</label>
+                        <input type="text" placeholder="Ingrese el estado" name="estado" id="direcion" class="form-control">
+                    </div>
+                    <input type="submit" value="Guardar Supervisión" class="btn btn-primary">
+                    <a href="lista_proveedor.php" class="btn btn-danger">Regresar</a>
+                </form>
+>>>>>>> 481afb0eeb9ca0542301cae91c3625376d91be0e
             </div>
         </div>
     </div>
+
 
 </div>
 <!-- /.container-fluid -->
