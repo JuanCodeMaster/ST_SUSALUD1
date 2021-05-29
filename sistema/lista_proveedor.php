14ed7888b96bf24c7d03c0afe5e17b761f8eba82
AndersonPM-HUB <79481049+AndersonPM-HUB@users.noreply.github.com>
2021-05-28 22:31:01 -0500

    Se realizo la interfaz de bandeja de supervisiones

--- sistema/lista_proveedor.php
+++ sistema/lista_proveedor.php
@@ -3,10 +3,17 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
+
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-4">
-		<h1 class="h3 mb-0 text-gray-800">Proveedores</h1>
+		<h1 class="h3 mb-0 text-gray-800">Registro de supervisión</h1>
 		<a href="registro_proveedor.php" class="btn btn-primary">Nuevo</a>
+
+		<nav class="navbar navbar-light bg-light">
+			<form class="form-inline">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			</form>
+		</nav>
 	</div>
 
 	<div class="row">
@@ -15,11 +22,16 @@
 				<table class="table table-striped table-bordered" id="table">
 					<thead class="thead-dark">
 						<tr>
-							<th>ID</th>
-							<th>RUC</th>
-							<th>PROVEEDOR</th>
-							<th>TELEFONO</th>
-							<th>DIRECCION</th>
+							<th>Cod.Plan</th>
+							<th>Año</th>
+							<th>Periodo</th>
+							<th>Entidad</th>
+							<th>T.supervisión</th>
+							<th>Modalidad</th>
+							<th>T.intervención</th>
+							<th>Avance</th>
+							<th>Estado</th>
+
 							<?php if ($_SESSION['rol'] == 1) { ?>
 							<th>ACCIONES</th>
 							<?php } ?>
@@ -38,6 +50,10 @@
 									<td><?php echo $data['contacto']; ?></td>
 									<td><?php echo $data['proveedor']; ?></td>
 									<td><?php echo $data['telefono']; ?></td>
+									<td><?php echo $data['direccion']; ?></td>
+									<td><?php echo $data['direccion']; ?></td>
+									<td><?php echo $data['direccion']; ?></td>
+									<td><?php echo $data['direccion']; ?></td>
 									<td><?php echo $data['direccion']; ?></td>
 									<?php if ($_SESSION['rol'] == 1) { ?>
 									<td>
